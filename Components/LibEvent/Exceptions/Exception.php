88f88154189fcd466dd31a36875242b0b199b173
Amal Samally <samara-pk@ya.ru>
2012-04-10 16:11:03 +0400

    First commit

---
+++ Components/LibEvent/Exceptions/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aza\Components\LibEvent\Exceptions;
+use RuntimeException as BaseExeption;
+
+/**
+ * Basic LibEvent exception.
+ *
+ * @project Anizoptera CMF
+ * @package system.libevent
+ * @version $Id: Exception.php 3253 2012-04-10 09:35:33Z samally $
+ * @author  Amal Samally <amal.samally at gmail.com>
+ * @license MIT
+ */
+class Exception extends BaseExeption {}

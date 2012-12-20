e30d4f2a5847ba0acd58036237057b573b9bdde7
Amal Samally <amal.samally@gmail.com>
2012-12-20 04:08:47 +0400

    Adding Anizoptera CMF subtree to 'framework/'

---
+++ Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aza\Components\LibEvent\Exceptions;
+use RuntimeException as BaseExeption;
+
+/**
+ * Basic LibEvent exception.
+ *
+ * @project Anizoptera CMF
+ * @package system.AzaLibEvent
+ * @author  Amal Samally <amal.samally at gmail.com>
+ * @license MIT
+ */
+class Exception extends BaseExeption {}
